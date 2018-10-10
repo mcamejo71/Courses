f1292da474b224702ff40e2b326014c85a20a248
mcamejo71 <mcamejo71@gmail.com>
2018-10-10 18:39:25 -0400

    Linq

--- Generics/Linq/Main.cs
+++ Generics/Linq/Main.cs
@@ -11,14 +11,77 @@ namespace Test1.Linq
 
         static void Main(string[] args)
         {
-            SelectMany();
+
+            //SelectMany();
             //GruopBy();
             //GruopingWithVariables();
-            GruopingAndNestedObjects();
+            //GruopingAndNestedObjects();
+            Totals();
 
             Console.ReadKey();
         }
 
+        static void Totals()
+        {
+            var t = from r in Formula1.GetChampions()
+                    where r.Country =="UK" || r.Country == "Brazil"
+                    select r;
+
+            foreach (var i in t)
+            {
+                Console.WriteLine($"{ i.FirstName,-5} {i.LastName,-20} {i.Country,20} {i.Wins}");
+            }
+            Console.WriteLine();
+
+            //var k = from r in t
+            //        group r by r.Country into g
+            //        let count = g.Count()
+            //        let sum = g.Sum(r => r.Wins)
+            //        let max = g.Max(r => r.Wins)
+            //        select new
+            //        {
+            //            Country = g.Key,
+            //            Count = count,
+            //            Sum = sum,
+            //            Max = max
+
+            //        };
+
+            var k = from r in t
+                    group r by r.Country into g
+                    select new
+                    {
+                        Country = g.Key,
+                        Count = g.Count(),
+                        Sum = g.Sum(r => r.Wins),
+                        Max = g.Max(r => r.Wins)
+                    };
+
+
+            foreach (var i in k)
+            {
+                Console.WriteLine($"{ i.Country,-15}{i.Count, -15}{i.Sum,-15}{i.Max,-15}");
+            }
+
+            Console.WriteLine();
+            //-----------
+            var q = t
+                .GroupBy(g => g.Country)
+                .Select(r => new {
+                    Country = r.Key,
+                    Count = r.Count(),
+                    Sum = r.Sum(r1 => r1.Wins),
+                    Max = r.Max(r1 => r1.Wins)
+                });
+
+
+            foreach (var i in q)
+            {
+                Console.WriteLine($"{ i.Country,-15}{i.Count,-15}{i.Sum,-15}{i.Max,-15}");
+            }
+
+
+        }
 
         static void SelectMany()
         {
@@ -47,7 +110,7 @@ namespace Test1.Linq
             }
         }
 
-        static void GruopBy()
+        static void GroupBy()
         {
             var Countries1 = from r in Formula1.GetChampions()
                              group r by r.Country into g
@@ -66,21 +129,21 @@ namespace Test1.Linq
             }
 
 
-            var Countries = Formula1.GetChampions()
-                .GroupBy(r => r.Country)
-                .OrderByDescending(q => q.Count())
-                .ThenBy(q => q.Key)
-                .Where(r => r.Count() > 2)
-                .Select(q => new
-                {
-                    Country = q.Key,
-                    Count = q.Count()
-                });
+            //var Countries = Formula1.GetChampions()
+            //    .GroupBy(r => r.Country)
+            //    .OrderByDescending(q => q.Count())
+            //    .ThenBy(q => q.Key)
+            //    .Where(r => r.Count() > 2)
+            //    .Select(q => new
+            //    {
+            //        Country = q.Key,
+            //        Count = q.Count()
+            //    });
 
-            foreach (var i in Countries)
-            {
-                Console.WriteLine($"{ i.Country,-50} {i.Count}");
-            }
+            //foreach (var i in Countries)
+            //{
+            //    Console.WriteLine($"{ i.Country,-50} {i.Count}");
+            //}
         }
 
         static void GruopingWithVariables()
