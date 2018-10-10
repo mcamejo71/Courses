045bb88f6a9161a3cdc0122b48690b3f79a74dd2
Jose Camejo <mcamejo71@gmail.com>
2018-10-09 23:26:33 -0400

    test3

--- Generics/Linq/Main.cs
+++ Generics/Linq/Main.cs
@@ -12,7 +12,10 @@ namespace Test1.Linq
         static void Main(string[] args)
         {
             //SelectMany();
-            GruopBy();
+            //GruopBy();
+            //GruopingWithVariables();
+            GruopingAndNestedObjects();
+
             Console.ReadKey();
         }
 
@@ -20,10 +23,10 @@ namespace Test1.Linq
         static void SelectMany()
         {
             var ferrariDrivers = from r in Formula1.GetChampions()
-                                  from c in r.Cars
-                                  where c == "Ferrari"
-                                  orderby r.LastName
-                                  select r;
+                                 from c in r.Cars
+                                 where c == "Ferrari"
+                                 orderby r.LastName
+                                 select r;
 
             foreach (var i in ferrariDrivers)
             {
@@ -46,6 +49,23 @@ namespace Test1.Linq
 
         static void GruopBy()
         {
+            var Countries1 = from r in Formula1.GetChampions()
+                             group r by r.Country into g
+                             orderby g.Count() descending, g.Key
+                             where g.Count() >= 2
+                             select new
+                             {
+                                 Country = g.Key,
+                                 Count = g.Count()
+                             };
+
+
+            foreach (var i in Countries1)
+            {
+                Console.WriteLine($"{ i.Country,-50} {i.Count}");
+            }
+
+
             var Countries = Formula1.GetChampions()
                 .GroupBy(r => r.Country)
                 .OrderByDescending(q => q.Count())
@@ -59,8 +79,112 @@ namespace Test1.Linq
 
             foreach (var i in Countries)
             {
-                Console.WriteLine($"{ i.Country, -50} {i.Count}");
+                Console.WriteLine($"{ i.Country,-50} {i.Count}");
             }
         }
+
+        static void GruopingWithVariables()
+        {
+            var countries = from r in Formula1.GetChampions()
+                            group r by r.Country into g
+                            let count = g.Count()
+                            orderby count descending, g.Key
+                            where count >= 2
+                            select new
+                            {
+                                Country = g.Key,
+                                Count = count
+                            };
+            foreach (var i in countries)
+            {
+                Console.WriteLine($"{ i.Country,-50} {i.Count}");
+            }
+
+            var Countries1 = Formula1.GetChampions()
+                .GroupBy(r => r.Country)
+                .Select(g => new {Group = g, Count = g.Count() })
+                .OrderByDescending(q => q.Count)
+                .ThenBy(g => g.Group.Key)
+                .Where(g => g.Count >= 2)
+                .Select(g => new
+                {
+                    Country = g.Group.Key,
+                    Count = g.Count
+                });
+
+            foreach (var i in Countries1)
+            {
+                Console.WriteLine($"{ i.Country,-50} {i.Count}");
+            }
+
+        }
+
+        static void GruopingAndNestedObjects()
+        {
+            var countries = from r in Formula1.GetChampions()
+                            group r by r.Country into g
+                            let count = g.Count()
+                            orderby count descending, g.Key
+                            where count >= 2
+                            select new
+                            {
+                                Country = g.Key,
+                                Count = count,
+                                Racers = from r1 in g
+                                         orderby r1.LastName
+                                         select new { r1.FirstName, r1.LastName }
+                            };
+
+
+            foreach (var i in countries)
+            {
+                Console.WriteLine($"{ i.Country,-5} {i.Count}");
+
+
+                foreach (var j in i.Racers)
+                {
+                    Console.WriteLine($"{ j.FirstName,-5} {j.LastName}");
+                }
+
+            }
+
+            //-------------------
+
+            var countries1 = Formula1.GetChampions()
+                .GroupBy(r => r.Country)
+                .Select(g => new
+                {
+                    Group = g,
+                    Key = g.Key,
+                    Count = g.Count()
+                })
+                .OrderByDescending(g => g.Count)
+                .ThenBy(g => g.Key)
+                .Where(g => g.Count >= 2)
+                .Select(g => new
+                {
+                    Country = g.Key,
+                    Count = g.Count,
+                    Racers = g.Group.OrderBy(r => r.LastName)
+                    .Select(r => new { r.FirstName, r.LastName })
+                });
+                            
+
+            foreach (var i in countries1)
+            {
+                Console.WriteLine($"{ i.Country,-5} {i.Count}");
+
+
+                foreach (var j in i.Racers)
+                {
+                    Console.WriteLine($"{ j.FirstName,-5} {j.LastName}");
+                }
+
+            }
+
+
+
+        }
+
     }
 }
