642ab927deb448ccffbf90ae270b96647ecf4225
mcamejo71 <mcamejo71@gmail.com>
2018-10-11 17:37:08 -0400

    Linq

--- Generics/Linq/Main.cs
+++ Generics/Linq/Main.cs
@@ -11,14 +11,160 @@ namespace Test1.Linq
 
         static void Main(string[] args)
         {
-
+            InnerJoinWithMethods();
+            //InnerJoin();
             //SelectMany();
             //GruopBy();
             //GruopingWithVariables();
             //GruopingAndNestedObjects();
-            Totals();
+            //Totals();
 
             Console.ReadKey();
+        }
+
+
+        static void InnerJoinWithMethods()
+        {
+            //var racers = Formula1.GetChampions()
+            //            .SelectMany(r => r.Years, (r1, year) =>
+            //            new 
+            //            {
+            //                Year = year,
+            //                Name = $"{r1.FirstName}{r1.LastName}"
+            //            });
+
+
+            //var teams = Formula1.GetConstructorChampions()
+            //            .SelectMany(t => t.Years, (t1, year) =>
+            //            new
+            //            {
+            //                Year = year,
+            //                Name = $"{t1.Name}"
+            //            });
+
+            //var racersAndTeams = (racers.Join(
+            //        teams, 
+            //        r => r.Year, 
+            //        t => t.Year, 
+            //        (r, t) => new
+            //        {
+            //            r.Year,
+            //            Champion = r.Name,
+            //            Construtor = t.Name
+            //        })).Take(10);
+
+
+            //---- In just one query-----
+
+            var racersAndTeams1 = (Formula1.GetChampions()
+                       .SelectMany(r => r.Years, (r1, year) =>
+                       new
+                       {
+                           Year = year,
+                           Name = $"{r1.FirstName}{r1.LastName}"
+                       })
+                       .Join(
+                        Formula1.GetConstructorChampions()
+                        .SelectMany(t => t.Years, (t1, year) =>
+                        new
+                        {
+                            Year = year,
+                            Name = $"{t1.Name}"
+                        }),
+                        r => r.Year,
+                        t => t.Year,
+                        (r, t) => new
+                        {
+                            r.Year,
+                            Champion = r.Name,
+                            Construtor = t.Name
+                        })).Take(10);
+
+
+            foreach (var i in racersAndTeams1)
+            {
+                Console.WriteLine($"{ i.Year,-5}{ i.Champion,-15}{i.Construtor}");
+            }
+            Console.WriteLine();
+
+        }
+
+        static void InnerJoin()
+        {
+            //var racers = from r in Formula1.GetChampions()
+            //             from y in r.Years
+            //             select new
+            //             {
+            //                 Year = y,
+            //                 Name =  r.FirstName + " " + r.LastName
+            //             };
+
+            //var teams = from t in Formula1.GetConstructorChampions()
+            //            from y in t.Years
+            //            select new
+            //            {
+            //                Year = y,
+            //                Name = t.Name
+            //            };
+
+
+            //var RacersAndTeams = (from r in racers
+            //                     join t in teams on r.Year equals t.Year
+            //                     orderby r.Year
+            //                     select new 
+            //                     {
+            //                         r.Year,
+            //                         Champion = r.Name,
+            //                         Construtor = t.Name
+            //                     }).Take(10);
+
+
+            //foreach (var i in RacersAndTeams)
+            //{
+            //    Console.WriteLine($"{ i.Year,-5}{ i.Champion,-15}{i.Construtor}");
+            //}
+            //Console.WriteLine();
+
+
+            //---- In just one query-----
+            var RacersAndTeams1 = ( from r in
+
+                                    from r1 in Formula1.GetChampions()
+                                    from yr in r1.Years
+                                    select new
+                                    {
+                                        Year = yr,
+                                        Name = r1.FirstName + " " + r1.LastName
+                                    }
+
+                                   join t in
+
+                                   from t1 in Formula1.GetConstructorChampions()
+                                   from yt in t1.Years
+                                   select new
+                                   {
+                                       Year = yt,
+                                       Name = t1.Name
+                                   }
+
+                                   on r.Year equals t.Year
+                                   orderby r.Year
+                                   select new
+                                   {
+                                       Year = r.Year,
+                                       Racer = r.Name,
+                                       Team = t.Name
+                                   }).Take(10);
+
+
+
+            foreach (var i in RacersAndTeams1)
+            {
+                Console.WriteLine($"{ i.Year,-5}{ i.Racer,-15}{i.Team}");
+            }
+            Console.WriteLine();
+
+
         }
 
         static void Totals()
