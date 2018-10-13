a06ddf041ed830942fa402febaa0eaf0fa28b60f
mcamejo71 <mcamejo71@gmail.com>
2018-10-13 18:28:46 -0400

    PLinq

---
+++ Generics/PLinq/Main.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test1.PLinq
+{
+    public class Main2
+    {
+        static IEnumerable<int> SampleData()
+        {
+            const int arraySize = 50000000;
+
+            var r = new Random();
+
+            return Enumerable.Range(0, arraySize).Select(x => r.Next(140)).ToList(); 
+        }
+
+        static void Main(string[] args)
+        {
+            var x = SampleData();
+
+
+            //var res = (from y in x
+            //           where Math.Log(y) < 4
+            //           select x).Sum();
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+
+
+    }
+}
