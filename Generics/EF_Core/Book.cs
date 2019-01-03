c14361db774d6d2f61e70afe1b3e91c13dfe7d3b
Jose Camejo <jcamejo@solishealthplans.com>
2019-01-03 15:58:02 -0500

    EF Core. Introduction.

---
+++ Generics/EF_Core/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Test1.EF_Core
+{
+    [Table("Books")]
+    public class Book
+    {
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(0)]
+        public string Publisher { get; set; }
+    }
+}
---
+++ Generics/EF_Core/BooksContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1.EF_Core
+{
+    public class BooksContext : DbContext
+    {
+        private const string conn = "server=LAPTOP-NMRO955A; database = WroxBooks;trusted_connection=true";
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder ob)
+        {
+            base.OnConfiguring(ob);
+            ob.UseSqlServer(conn);
+        }
+
+    }
+}
